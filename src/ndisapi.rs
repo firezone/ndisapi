e9492a9369f0db55218e0a64ed09d4569cf8be19
Vadim Smirnov <vadim@ntkernel.com>
2023-01-04 16:28:59 +0100

    Corrected previous commit

--- src/ndisapi.rs
+++ src/ndisapi.rs
@@ -157,7 +157,7 @@ impl Ndisapi {
             result = DeviceIoControl(
                 self.driver_handle,
                 driver::IOCTL_NDISRD_READ_PACKET,
-                Some(&eth_request as *const driver::driver::EthMRequest<N> as *const std::ffi::c_void),
+                Some(&eth_request as *const driver::EthRequest as *const std::ffi::c_void),
                 size_of::<driver::EthRequest>() as u32,
                 Some(std::mem::transmute::<
                     &driver::EthRequest,
